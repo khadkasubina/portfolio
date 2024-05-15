5659e518852d24ffad5da4b4d5bff186a54460cb
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-05-15 22:17:32 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+// function printNumberTriangle(rows) {
+//     for (let i = 1; i <= rows; i++) {
+//       let row = '';
+//       for (let j = 1; j <= i; j++) {
+//         row += j + ' ';
+//       }
+//       console.log(row);
+//     }
+//   }
+  
+//   const rows = 5;
+//   console.log("Right-angled Triangle:");
+//   printNumberTriangle(rows);
+  
+//   function printNumberReverseTriangle(rows) {
+//     for (let i = rows; i >= 1; i--) {
+//       let row = '';
+//       for (let j = 1; j <= i; j++) {
+//         row += j + ' ';
+//       }
+//       console.log(row);
+//     }
+//   }
+  
+//   console.log("\nReverse Right-angled Triangle:");
+//   printNumberReverseTriangle(rows);
+
+// function isPalindrome(str) { 
+//     return str === str.split('').reverse().join(''); 
+  
+//   }
+//   let a = "abcba";
+//   let c = isPalindrome(a);
+//   console.log(c);
